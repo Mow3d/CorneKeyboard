64ea28ba8d56f21de5ee3adf3a9db4fe8b8c9b5e
Mowgly Schwarzwildhirsch <mowgly@gmail.com>
2021-12-28 13:46:08 -0500

    Add files via upload
    Setup for cornekeyboard (Mow COnfiguration)

---
+++ rules.mk
@@ -0,0 +1,16 @@
+#OLED_ENABLE = yes
+#OLED_DRIVER = SSD1306
+
+#MOUSEKEY_ENABLE     = no     # Mouse keys
+#RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+
+#RGB_MATRIX_ENABLE   = WS2812
+#EXTRAKEY_ENABLE     = yes
+#TAP_DANCE_ENEBLE    = yes
+
+MOUSEKEY_ENABLE     = no     # Mouse keys
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = no         # Enable VIA
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+LTO_ENABLE  = yes
